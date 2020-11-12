5de67eafff9b26b7095a1ccb93ca603f44eae5ba
Nick <nbisigna@gmail.com>
2020-11-12 00:55:08 -0500

    Enginer Part#2

--- basic.js
+++ basic.js
@@ -53,6 +53,9 @@ function mat4x4() {
 function Engine3d() {
   var meshCube = new mesh();
   var matProj = new mat4x4();
+
+  var vCamera = new vec3d();
+
   var fTheta = 0;
   this.MultiplyMatrixVector = function (i, o, m) {
     o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
@@ -67,31 +70,32 @@ function Engine3d() {
     return o;
   };
   this.OnUserCreate = function () {
-    meshCube.tris = [
-      // SOUTH
-      new triangle(0, 0, 0, 0, 1, 0, 1, 1, 0),
-      new triangle(0, 0, 0, 1, 1, 0, 1, 0, 0),
+    meshCube.tris = spaceship;
+    // [
+    //   // SOUTH
+    //   new triangle(0, 0, 0, 0, 1, 0, 1, 1, 0),
+    //   new triangle(0, 0, 0, 1, 1, 0, 1, 0, 0),
 
-      // EAST
-      new triangle(1, 0, 0, 1, 1, 0, 1, 1, 1),
-      new triangle(1, 0, 0, 1, 1, 1, 1, 0, 1),
+    //   // EAST
+    //   new triangle(1, 0, 0, 1, 1, 0, 1, 1, 1),
+    //   new triangle(1, 0, 0, 1, 1, 1, 1, 0, 1),
 
-      // NORTH
-      new triangle(1, 0, 1, 1, 1, 1, 0, 1, 1),
-      new triangle(1, 0, 1, 0, 1, 1, 0, 0, 1),
+    //   // NORTH
+    //   new triangle(1, 0, 1, 1, 1, 1, 0, 1, 1),
+    //   new triangle(1, 0, 1, 0, 1, 1, 0, 0, 1),
 
-      // WEST
-      new triangle(0, 0, 1, 0, 1, 1, 0, 1, 0),
-      new triangle(0, 0, 1, 0, 1, 0, 0, 0, 0),
+    //   // WEST
+    //   new triangle(0, 0, 1, 0, 1, 1, 0, 1, 0),
+    //   new triangle(0, 0, 1, 0, 1, 0, 0, 0, 0),
 
-      // TOP
-      new triangle(0, 1, 0, 0, 1, 1, 1, 1, 1),
-      new triangle(0, 1, 0, 1, 1, 1, 1, 1, 0),
+    //   // TOP
+    //   new triangle(0, 1, 0, 0, 1, 1, 1, 1, 1),
+    //   new triangle(0, 1, 0, 1, 1, 1, 1, 1, 0),
 
-      // BOTTOM
-      new triangle(1, 0, 1, 0, 0, 1, 0, 0, 0),
-      new triangle(1, 0, 1, 0, 0, 0, 1, 0, 0),
-    ];
+    //   // BOTTOM
+    //   new triangle(1, 0, 1, 0, 0, 1, 0, 0, 0),
+    //   new triangle(1, 0, 1, 0, 0, 0, 1, 0, 0),
+    // ];
 
     var fNear = 0.1;
     var fFar = 1000;
@@ -129,6 +133,8 @@ function Engine3d() {
     matRotX.m[2][1] = -Math.sin(fTheta * 0.5);
     matRotX.m[2][2] = Math.cos(fTheta * 0.5);
     matRotX.m[3][3] = 1;
+
+    var vecTrianglesToRaster = [];
 
     for (var i = 0; i < meshCube.tris.length; i++) {
       var triProjected = new triangle(),
@@ -170,48 +176,118 @@ function Engine3d() {
       );
 
       triTranslated = triRotatedZX;
-      triTranslated.p[0].z = triRotatedZX.p[0].z + 3;
-      triTranslated.p[1].z = triRotatedZX.p[1].z + 3;
-      triTranslated.p[2].z = triRotatedZX.p[2].z + 3;
+      triTranslated.p[0].z = triRotatedZX.p[0].z + 8;
+      triTranslated.p[1].z = triRotatedZX.p[1].z + 8;
+      triTranslated.p[2].z = triRotatedZX.p[2].z + 8;
 
-      triProjected.p[0] = this.MultiplyMatrixVector(
-        triTranslated.p[0],
-        triProjected.p[0],
-        matProj
-      );
-      triProjected.p[1] = this.MultiplyMatrixVector(
-        triTranslated.p[1],
-        triProjected.p[1],
-        matProj
-      );
-      triProjected.p[2] = this.MultiplyMatrixVector(
-        triTranslated.p[2],
-        triProjected.p[2],
-        matProj
-      );
+      var normal = new vec3d(),
+        line1 = new vec3d(),
+        line2 = new vec3d();
+      line1.x = triTranslated.p[1].x - triTranslated.p[0].x;
+      line1.y = triTranslated.p[1].y - triTranslated.p[0].y;
+      line1.z = triTranslated.p[1].z - triTranslated.p[0].z;
 
-      triProjected.p[0].x += 1;
-      triProjected.p[0].y += 1;
-      triProjected.p[1].x += 1;
-      triProjected.p[1].y += 1;
-      triProjected.p[2].x += 1;
-      triProjected.p[2].y += 1;
-      triProjected.p[0].x *= 0.5 * Width;
-      triProjected.p[0].y *= 0.5 * Height;
-      triProjected.p[1].x *= 0.5 * Width;
-      triProjected.p[1].y *= 0.5 * Height;
-      triProjected.p[2].x *= 0.5 * Width;
-      triProjected.p[2].y *= 0.5 * Height;
+      line2.x = triTranslated.p[2].x - triTranslated.p[0].x;
+      line2.y = triTranslated.p[2].y - triTranslated.p[0].y;
+      line2.z = triTranslated.p[2].z - triTranslated.p[0].z;
 
+      normal.x = line1.y * line2.z - line1.z * line2.y;
+      normal.y = line1.z * line2.x - line1.x * line2.z;
+      normal.z = line1.x * line2.y - line1.y * line2.x;
+
+      var l = Math.sqrt(
+        normal.x * normal.x + normal.y * normal.y + normal.z * normal.z
+      );
+      normal.x /= l;
+      normal.y /= l;
+      normal.z /= l;
+
+      if (
+        normal.x * (triTranslated.p[0].x - vCamera.x) +
+          normal.y * (triTranslated.p[0].y - vCamera.y) +
+          normal.z * (triTranslated.p[0].z - vCamera.z) <
+        0
+      ) {
+        var light_direction = new vec3d(0, 1, -1);
+        var l = Math.sqrt(
+          light_direction.x * light_direction.x +
+            light_direction.y * light_direction.y +
+            light_direction.z * light_direction.z
+        );
+        light_direction.x /= l;
+        light_direction.y /= l;
+        light_direction.z /= l;
+
+        // How similar is normal to light direction
+        var dp =
+          normal.x * light_direction.x +
+          normal.y * light_direction.y +
+          normal.z * light_direction.z;
+
+        // Choose console colours as required (much easier with RGB)
+
+        var g = ((dp * 256) | 0).toString(16);
+        triProjected.c = '#' + g + g + g;
+
+        triProjected.p[0] = this.MultiplyMatrixVector(
+          triTranslated.p[0],
+          triProjected.p[0],
+          matProj
+        );
+        triProjected.p[1] = this.MultiplyMatrixVector(
+          triTranslated.p[1],
+          triProjected.p[1],
+          matProj
+        );
+        triProjected.p[2] = this.MultiplyMatrixVector(
+          triTranslated.p[2],
+          triProjected.p[2],
+          matProj
+        );
+
+        triProjected.p[0].x += 1;
+        triProjected.p[0].y += 1;
+        triProjected.p[1].x += 1;
+        triProjected.p[1].y += 1;
+        triProjected.p[2].x += 1;
+        triProjected.p[2].y += 1;
+        triProjected.p[0].x *= 0.5 * Width;
+        triProjected.p[0].y *= 0.5 * Height;
+        triProjected.p[1].x *= 0.5 * Width;
+        triProjected.p[1].y *= 0.5 * Height;
+        triProjected.p[2].x *= 0.5 * Width;
+        triProjected.p[2].y *= 0.5 * Height;
+        vecTrianglesToRaster.push(triProjected);
+      }
+    }
+    vecTrianglesToRaster.sort((t1, t2) => {
+      var z1 = t1.p[0].z + t1.p[1].z + t1.p[2].z / 3;
+      var z2 = t2.p[0].z + t2.p[1].z + t2.p[2].z / 3;
+      return z1 > z2;
+    });
+    for (var i = 0; i < vecTrianglesToRaster.length; i++) {
       ctx.beginPath();
-      ctx.moveTo(triProjected.p[0].x, triProjected.p[0].y);
-      ctx.lineTo(triProjected.p[1].x, triProjected.p[1].y);
-      ctx.lineTo(triProjected.p[2].x, triProjected.p[2].y);
-      ctx.lineTo(triProjected.p[0].x, triProjected.p[0].y);
+      ctx.moveTo(
+        vecTrianglesToRaster[i].p[0].x,
+        vecTrianglesToRaster[i].p[0].y
+      );
+      ctx.lineTo(
+        vecTrianglesToRaster[i].p[1].x,
+        vecTrianglesToRaster[i].p[1].y
+      );
+      ctx.lineTo(
+        vecTrianglesToRaster[i].p[2].x,
+        vecTrianglesToRaster[i].p[2].y
+      );
+      ctx.lineTo(
+        vecTrianglesToRaster[i].p[0].x,
+        vecTrianglesToRaster[i].p[0].y
+      );
       ctx.closePath();
-      ctx.strokeStyle = 'white';
-      //   ctx.fillStyle = 'white';
-      ctx.stroke();
+      // ctx.strokeStyle = 'black';
+      // ctx.stroke();
+      ctx.fillStyle = vecTrianglesToRaster[i].c;
+      ctx.fill();
     }
   };
   this.OnUserCreate();
---
+++ fs.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const readline = require('readline');
+var filename = 'spaceship';
+function triangle(a, b, c, d, e, f, g, h, i) {
+  this.p = [new vec3d(a, b, c), new vec3d(d, e, f), new vec3d(g, h, i)];
+}
+var triangles = [];
+var vertices = [];
+function vec3d(x = 0, y = 0, z = 0, w = 1) {
+  this.x = x;
+  this.y = y;
+  this.z = z;
+  this.w = w;
+}
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream('./' + filename + '.obj'),
+  output: false,
+  console: false,
+});
+
+readInterface.on('line', function (line) {
+  var arr = line.split(' ');
+  if (arr[0] == 'v') {
+    vertices.push(new vec3d(Number(arr[1]), Number(arr[2]), Number(arr[3])));
+  } else if (arr[0] == 'f') {
+    one = Number(arr[1]) - 1;
+    two = Number(arr[2]) - 1;
+    three = Number(arr[3]) - 1;
+    triangles.push(
+      new triangle(
+        vertices[one].x,
+        vertices[one].y,
+        vertices[one].z,
+        vertices[two].x,
+        vertices[two].y,
+        vertices[two].z,
+        vertices[three].x,
+        vertices[three].y,
+        vertices[three].z
+      )
+    );
+  }
+});
+
+readInterface.on('close', () => {
+  fs.writeFile('./' + filename + '.js', JSON.stringify(triangles), (err) => {
+    console.error(err);
+  });
+});
